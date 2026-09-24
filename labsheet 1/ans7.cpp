87026f227ef9ea812b00e2bd96d835574ef93bcb
kush-117 <kush750529@gmail.com>
2026-09-24 20:41:20 +0530

    Update

--- labsheet 1/ans7.cpp
+++ labsheet 1/ans7.cpp
@@ -9,6 +9,25 @@ int main() {
     int Arr2[] = {11, 3, 7, 1};
     int n1 = sizeof(Arr1) / sizeof(Arr1[0]);
     int n2 = sizeof(Arr2) / sizeof(Arr2[0]);
-    
+    bool isSubset = true;
+    for (int i = 0; i < n2; i++) {
+        bool found = false;
+        for (int j = 0; j < n1; j++) {
+            if (Arr2[i] == Arr1[j]) {
+                found = true;
+                break;
+            }
+        }
+        if (!found) {
+            isSubset = false;
+            break;
+        }
+    }
+    if (isSubset) {
+        cout << "Yes" << endl;
+    } else {
+        cout << "No" << endl;
+    }
+
     return 0;
 }
